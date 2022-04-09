66de3e1de183b0076f27ae7da1fda0fe5ea96a05
Guilherme Fill <guilhermefill@gmail.com>
2022-04-09 16:02:50 +0200

    iteration 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import navbar from '../assets/navbar.png';
+
 const Header = () => {
 	return (
 		<header>
 			<Link to="/">
-				<img src="" alt="" />
+				<img src={navbar} alt="" />
 			</Link>
 		</header>
 	);
---
+++ src/pageComponents/RandomBeer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+const RandomBeer = () => {
+	return (
+		<div>
+			<Header />
+		</div>
+	);
+};
+
+export default RandomBeer;
